fbdd276caf7079e4067dd35851a5aa350d2994fe
chakrit-jsd <chakritabv@gmail.com>
2023-06-03 17:46:40 +0700

    Edit: slack login

--- src/middlewares/passposts/usePassport.js
+++ src/middlewares/passposts/usePassport.js
@@ -1,30 +1,30 @@
 const passport = require('passport')
 const Users = require('../../models/Users.schema')
 const SlackStrategy = require('passport-slack').Strategy
-// const LocalStrategy = require('passport-local').Strategy
+const LocalStrategy = require('passport-local').Strategy
 // const localStrategy = require('./strategies/local.strategy')
 // const slackStrategy = require('./strategies/slack.stategy')
 const { SLACK_CLIENT_ID, SCACK_CLIENT_SECRET, SLACK_CALLBACK } = process.env
-// passport.use('local', new LocalStrategy({
-//   usernameField: 'email',
-//   passwordField: 'password',
-//   session: true
-// }, async (email, password, next) => {
-//   try {
-//     const user = await Users.findOne({ email })
-//     if (!user) {
-//       throw {resError: [404, 'Email Not Found']}
-//     }
-//     const result = await user.comparePassword(password)
-//     if (!result) {
-//       throw {resError: [404, 'Password Incorrect']}
-//     }
+passport.use(new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password',
+  session: true
+}, async (email, password, next) => {
+  try {
+    const user = await Users.findOne({ email })
+    if (!user) {
+      throw {resError: [404, 'Email Not Found']}
+    }
+    const result = await user.comparePassword(password)
+    if (!result) {
+      throw {resError: [404, 'Password Incorrect']}
+    }
 
-//     return next(null, user)
-//   } catch (error) {
-//     return next(error)
-//   }
-// }))
+    return next(null, user)
+  } catch (error) {
+    return next(error)
+  }
+}))
 
 passport.use(new SlackStrategy({
   clientID: SLACK_CLIENT_ID,
