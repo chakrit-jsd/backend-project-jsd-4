85e9d9cda7a33ec950fd0077727208a71436cd1d
chakrit-jsd <chakritabv@gmail.com>
2023-09-10 11:52:10 +0000

    add: all file from server

--- src/middlewares/passposts/strategies/slack.stategy.js
+++ src/middlewares/passposts/strategies/slack.stategy.js
@@ -1,20 +1,20 @@
-const SlackStrategy = require('passport-slack').Strategy
-const Users = require('../../../models/Users.schema')
+//const SlackStrategy = require('passport-slack').Strategy
+//const Users = require('../../../models/Users.schema')
 
-const { SLACK_CLIENT_ID, SCACK_CLIENT_SECRET, SLACK_CALLBACK } = process.env
+//const { SLACK_CLIENT_ID, SCACK_CLIENT_SECRET, SLACK_CALLBACK } = process.env
 
-const slackStrategy = new SlackStrategy({
-  clientID: SLACK_CLIENT_ID,
-  clientSecret: SCACK_CLIENT_SECRET,
-  callbackURL: SLACK_CALLBACK,
-  skipUserProfile: false,
-  scope: ['identity.basic', 'identity.email', 'identity.avatar']
-}, (accessToken, refreshToken, profile, done) => {
-      console.log('au 1')
-      console.log(accessToken)
-      console.log(profile.id)
+//const slackStrategy = new SlackStrategy({
+  //clientID: SLACK_CLIENT_ID,
+///  clientSecret: SCACK_CLIENT_SECRET,
+//  callbackURL: SLACK_CALLBACK,
+//  skipUserProfile: false,
+//  scope: ['identity.basic', 'identity.email', 'identity.avatar']
+//}, (accessToken, refreshToken, profile, done) => {
+//      console.log('au 1')
+//      console.log(accessToken)
+//      console.log(profile.id)
 
-      done(null, profile)
-  })
+//      done(null, profile)
+//  })
 
-module.exports = slackStrategy
+//module.exports = slackStrategy
